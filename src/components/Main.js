20ebfc47a923081eb7f937613888ff3cda57a236
Vaikunthkeshav <vakr3628@colorado.edu>
2025-05-31 15:39:38 -0600

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,6 @@ import styled, { keyframes } from 'styled-components'
 import LogoComponent from '../subComponents/LogoComponent'
 import PowerButton from '../subComponents/PowerButton'
 import SocialIcons from '../subComponents/SocialIcons'
-import ParticleComponent from '../subComponents/ParticleComponent'
 import { YinYang } from './AllSvgs'
 import Intro from './Intro'
 
@@ -14,6 +13,7 @@ background: ${props => props.theme.body};
 width: 100vw;
 height: 100vh;
 overflow:hidden;
+
 position: relative;
 
 h2,h3,h4,h5,h6{
@@ -24,8 +24,6 @@ h2,h3,h4,h5,h6{
 
 const Container = styled.div`
 padding: 2rem;
-position: relative;
-z-index: 2;
 `
 
 const Contact = styled.a`
@@ -34,9 +32,8 @@ position: absolute;
 top: 2rem;
 right: calc(1rem + 2vw);
 text-decoration: none;
-z-index:4;
+z-index:1;
 `
-
 const BLOG = styled(NavLink)`
 color: ${props => props.theme.text};
 position: absolute;
@@ -44,17 +41,17 @@ top: 50%;
 right: calc(1rem + 2vw);
 transform: rotate(90deg) translate(-50%, -50%);
 text-decoration: none;
-z-index:4;
+z-index:1;
 `
-
 const WORK = styled(NavLink)`
 color: ${props => props.click ? props.theme.body : props.theme.text};
+
 position: absolute;
 top: 50%;
 left: calc(1rem + 2vw);
 transform: translate(-50%, -50%) rotate(-90deg) ;
 text-decoration: none;
-z-index:4;
+z-index:1;
 `
 
 const BottomBar = styled.div`
@@ -63,21 +60,20 @@ bottom: 1rem;
 left: 0;
 right: 0;
 width: 100%;
+
 display: flex;
 justify-content: space-evenly;
-z-index:4;
 `
 
 const ABOUT = styled(NavLink)`
 color: ${props => props.click ? props.theme.body : props.theme.text};
 text-decoration: none;
-z-index:4;
+z-index:1;
 `
-
 const SKILLS = styled(NavLink)`
 color: ${props => props.theme.text};
 text-decoration: none;
-z-index:4;
+z-index:1;
 `
 
 const rotate = keyframes`
@@ -98,7 +94,6 @@ border: none;
 outline: none;
 background-color: transparent;
 cursor: pointer;
-z-index: 4;
 
 display: flex;
 flex-direction: column;
@@ -113,7 +108,6 @@ transition: all 1s ease;
 &>:last-child{
     display: ${props => props.click ? 'none' :'inline-block'  };
     padding-top: 1rem;
-    color: ${props => props.theme.text};
 }
 `
 
@@ -129,125 +123,112 @@ z-index:1;
 transition: height 0.5s ease, width 1s ease 0.5s;
 `
 
-const IntroWrapper = styled.div`
-position: relative;
-z-index: 3;
-`
-
 const Main = () => {
+
     const [click, setClick] = useState(true);
 
     const handleClick = () => setClick(!click);
 
     return (
         <MainContainer>
-            {/* Dense Particle Network Background */}
-            <ParticleComponent theme={click ? 'dark' : 'light'} />
-            
-            <DarkDiv click={click}/>
+         <DarkDiv   click={click}/>
             <Container>
-                <PowerButton />
-                <LogoComponent theme={click ? 'dark' :'light'}/>
-                <SocialIcons theme={click ? 'dark' :'light'} />
-               
-                <Center click={click}>
-                    <YinYang onClick={()=> handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
-                    <span>click here</span>
-                </Center>
+            <PowerButton />
+            <LogoComponent theme={click ? 'dark' :'light'}/>
+            <SocialIcons theme={click ? 'dark' :'light'} />
+           
+            <Center click={click}>
+                <YinYang  onClick={()=> handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
+                <span>click here</span>
+            </Center>
 
-                <Contact target="_blank" href="mailto:vakr3628@colorado.edu">
-                    <motion.h2
-                    initial={{
-                        y:-200,
-                        transition: { type:'spring', duration: 1.5, delay:1}
-                    }}
-                    animate={{
-                        y:0,
-                        transition: { type:'spring', duration: 1.5, delay:1}
-                    }}
-                    whileHover={{scale: 1.1}}
-                    whileTap={{scale: 0.9}}
-                    >
-                        Say hi..
-                    </motion.h2>
-                </Contact>
+            <Contact target="_blank" href="mailto:vakr3628@colorado.edu">
+                <motion.h2
+                initial={{
+                    y:-200,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                animate={{
+                    y:0,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
                 
-                <BLOG to="/blog">
-                    <motion.h2
-                    initial={{
-                        y:-200,
-                        transition: { type:'spring', duration: 1.5, delay:1}
-                    }}
-                    animate={{
-                        y:0,
-                        transition: { type:'spring', duration: 1.5, delay:1}
-                    }}
-                    whileHover={{scale: 1.1}}
-                    whileTap={{scale: 0.9}}
-                    >
-                        Blog
-                    </motion.h2>
-                </BLOG>
-                
-                <WORK to="/work" click={+click}>
-                    <motion.h2
-                    initial={{
-                        y:-200,
-                        transition: { type:'spring', duration: 1.5, delay:1}
-                    }}
-                    animate={{
-                        y:0,
-                        transition: { type:'spring', duration: 1.5, delay:1}
-                    }}
-                     whileHover={{scale: 1.1}}
-                    whileTap={{scale: 0.9}}
-                    >
-                        Work
-                    </motion.h2>
-                </WORK>
-                
-                <BottomBar>
-                    <ABOUT to="/about" click={+click}>
-                        <motion.h2
-                        initial={{
-                            y:200,
-                            transition: { type:'spring', duration: 1.5, delay:1}
-                        }}
-                        animate={{
-                            y:0,
-                            transition: { type:'spring', duration: 1.5, delay:1}
-                        }}
-                         whileHover={{scale: 1.1}}
-                        whileTap={{scale: 0.9}}
-                        >
-                            About.
-                        </motion.h2>
-                    </ABOUT>
-                    
-                    <SKILLS to="/skills">
-                        <motion.h2
-                        initial={{
-                            y:200,
-                            transition: { type:'spring', duration: 1.5, delay:1}
-                        }}
-                        animate={{
-                            y:0,
-                            transition: { type:'spring', duration: 1.5, delay:1}
-                        }}
-                         whileHover={{scale: 1.1}}
-                        whileTap={{scale: 0.9}}
-                        >
-                            My Skills.
-                        </motion.h2>
-                    </SKILLS>
-                </BottomBar>
+                >
+                    Say hi..
+                </motion.h2>
+            </Contact>
+            <BLOG to="/blog">
+                <motion.h2
+                initial={{
+                    y:-200,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                animate={{
+                    y:0,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
+                >
+                    Blog
+                </motion.h2>
+            </BLOG>
+            <WORK to="/work" click={+click}>
+                <motion.h2
+                initial={{
+                    y:-200,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                animate={{
+                    y:0,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                 whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
+                >
+                    Work
+                </motion.h2>
+            </WORK>
+            <BottomBar>
+            <ABOUT to="/about" click={+click}>
+                <motion.h2
+                initial={{
+                    y:200,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                animate={{
+                    y:0,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                 whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
+                >
+                    About.
+                </motion.h2>
+            </ABOUT>
+            <SKILLS to="/skills">
+                <motion.h2
+                initial={{
+                    y:200,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                animate={{
+                    y:0,
+                    transition: { type:'spring', duration: 1.5, delay:1}
+                }}
+                 whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
+                >
+                    My Skills.
+                </motion.h2>
+            </SKILLS>
+
+            </BottomBar>
+
             </Container>
-            
-            {click ? (
-                <IntroWrapper>
-                    <Intro click={click} />
-                </IntroWrapper>
-            ) : null }
+            {click ? <Intro click={click} /> : null }
         </MainContainer>
     )
 }
