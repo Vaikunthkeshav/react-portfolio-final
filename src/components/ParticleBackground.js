3ff5c1d5da2c5f4e575b082954bee574867e3811
Vaikunthkeshav <vakr3628@colorado.edu>
2025-05-31 14:48:28 -0600

    Create ParticleBackground.js

---
+++ src/components/ParticleBackground.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ParticleCanvas = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  pointer-events: none;
+  background: transparent;
+`;
+
+const ParticleBackground = () => {
+  return <ParticleCanvas />;
+};
+
+export default ParticleBackground;
