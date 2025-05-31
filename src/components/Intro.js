096663300515b48ff49dc067fa0435a650a3a00c
Vaikunthkeshav <vakr3628@colorado.edu>
2025-05-31 14:09:48 -0600

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,20 +3,14 @@ import styled from 'styled-components'
 import {motion} from 'framer-motion'
 import Me from '../assets/Images/profile-img.png'
 
-
 const Box = styled(motion.div)`
-
 position: absolute;
 left: 50%;
 top: 50%;
 transform: translate(-50%, -50%);
-
-
 width: 65vw;
 height:55vh;
 display: flex;
-
-
 background: linear-gradient(
     to right,
     ${props => props.theme.body} 50%,
@@ -29,11 +23,9 @@ background: linear-gradient(
 background-size: 100% 2px;
     border-left: 2px solid ${props => props.theme.body};
     border-right: 2px solid ${props => props.theme.text};
-
-
     z-index:1;
-
 `
+
 const SubBox = styled.div`
 width: 50%;
 position: relative;
@@ -54,7 +46,6 @@ font-size: calc(1em + 1.5vw);
 color: ${props => props.theme.body};
 padding: 2rem;
 cursor: pointer;
-
 display: flex;
 flex-direction: column;
 justify-content: space-evenly;
@@ -63,11 +54,7 @@ justify-content: space-evenly;
     color: ${props => `rgba(${props.theme.bodyRgba},0.6)` };
     font-size: calc(0.5rem + 1.5vw);
     font-weight:300;
-
 }
-
-
-
 `
 
 const Intro = () => {
@@ -80,8 +67,8 @@ const Intro = () => {
             <SubBox>
                 <Text>
                     <h1>Hi,</h1>
-                    <h3>I'm CodeBucks.</h3>
-                    <h6>I design and Code simple yet beautiful websites.</h6>
+                    <h3>I'm Vaikunth Keshav.</h3>
+                    <h6>I'm pursuing my M.S. in Mechanical Engineering at University of Colorado Boulder.</h6>
                 </Text>
             </SubBox>
             <SubBox>
