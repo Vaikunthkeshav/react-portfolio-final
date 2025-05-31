664ef651148cdf5e8ee3e001131da82d2f6212d4
Vaikunthkeshav <vakr3628@colorado.edu>
2025-05-31 14:37:11 -0600

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,7 @@ import styled, { keyframes } from 'styled-components'
 import LogoComponent from '../subComponents/LogoComponent'
 import PowerButton from '../subComponents/PowerButton'
 import SocialIcons from '../subComponents/SocialIcons'
-import { YinYang } from './AllSvgs'
 import Intro from './Intro'
-;
-
 
 const MainContainer = styled.div`
 background: ${props => props.theme.body};
@@ -78,39 +75,109 @@ text-decoration: none;
 z-index:1;
 `
 
-const rotate = keyframes`
-from{
-    transform: rotate(0);
-}
-to{
-    transform: rotate(360deg);
-}
+// Particle Animation Keyframes
+const float = keyframes`
+  0%, 100% { transform: translateY(0px) rotate(0deg); }
+  33% { transform: translateY(-10px) rotate(120deg); }
+  66% { transform: translateY(5px) rotate(240deg); }
 `
 
-const Center = styled.button`
+const pulse = keyframes`
+  0%, 100% { opacity: 0.7; transform: scale(1); }
+  50% { opacity: 1; transform: scale(1.1); }
+`
+
+const orbit = keyframes`
+  from { transform: rotate(0deg) translateX(50px) rotate(0deg); }
+  to { transform: rotate(360deg) translateX(50px) rotate(-360deg); }
+`
+
+// Particle Button Container
+const ParticleButton = styled.button`
 position: absolute;
 top: ${props => props.click ? '85%' :'50%'  };
 left: ${props => props.click ? '92%' :'50%'  };
 transform: translate(-50%,-50%);
 border: none;
 outline: none;
-background-color: transparent;
+background: transparent;
 cursor: pointer;
+width: ${props => props.click ? '120px' : '200px'};
+height: ${props => props.click ? '120px' : '200px'};
+transition: all 1s ease;
 
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-transition: all 1s ease;
+`
 
-&>:first-child{
-    animation: ${rotate} infinite 1.5s linear;
+// Central Particle
+const CentralParticle = styled.div`
+width: 20px;
+height: 20px;
+background: ${props => props.theme.text};
+border-radius: 50%;
+position: relative;
+animation: ${pulse} 2s infinite;
+box-shadow: 0 0 20px ${props => props.theme.text};
+`
+
+// Orbiting Particles
+const OrbitingParticle = styled.div`
+position: absolute;
+width: 8px;
+height: 8px;
+background: ${props => props.theme.text};
+border-radius: 50%;
+opacity: 0.8;
+animation: ${orbit} 4s infinite linear;
+box-shadow: 0 0 10px ${props => props.theme.text};
+
+&:nth-child(2) {
+  animation-delay: -1s;
+  animation-duration: 3s;
 }
 
-&>:last-child{
-    display: ${props => props.click ? 'none' :'inline-block'  };
-    padding-top: 1rem;
+&:nth-child(3) {
+  animation-delay: -2s;
+  animation-duration: 5s;
 }
+
+&:nth-child(4) {
+  animation-delay: -0.5s;
+  animation-duration: 3.5s;
+}
+
+&:nth-child(5) {
+  animation-delay: -1.5s;
+  animation-duration: 4.5s;
+}
+`
+
+// Floating Particles
+const FloatingParticle = styled.div`
+position: absolute;
+width: 4px;
+height: 4px;
+background: ${props => props.theme.text};
+border-radius: 50%;
+opacity: 0.6;
+animation: ${float} 6s infinite ease-in-out;
+
+&:nth-child(6) { top: -30px; left: -20px; animation-delay: -1s; }
+&:nth-child(7) { top: -20px; right: -30px; animation-delay: -2s; }
+&:nth-child(8) { bottom: -30px; left: -15px; animation-delay: -3s; }
+&:nth-child(9) { bottom: -20px; right: -25px; animation-delay: -4s; }
+&:nth-child(10) { top: 10px; left: -40px; animation-delay: -5s; }
+&:nth-child(11) { top: 20px; right: -35px; animation-delay: -0.5s; }
+`
+
+const ClickText = styled.span`
+display: ${props => props.click ? 'none' :'block'  };
+margin-top: 1rem;
+color: ${props => props.theme.text};
+font-size: 0.8rem;
 `
 
 const DarkDiv = styled.div`
@@ -125,10 +192,9 @@ z-index:1;
 transition: height 0.5s ease, width 1s ease 0.5s;
 `
 
-
 const Main = () => {
 
-    const [click, setClick] = useState(false);
+    const [click, setClick] = useState(true);
 
     const handleClick = () => setClick(!click);
 
@@ -140,12 +206,23 @@ const Main = () => {
             <LogoComponent theme={click ? 'dark' :'light'}/>
             <SocialIcons theme={click ? 'dark' :'light'} />
            
-            <Center click={click}>
-                <YinYang  onClick={()=> handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
-                <span>click here</span>
-            </Center>
+            <ParticleButton click={click} onClick={handleClick}>
+                <CentralParticle />
+                <OrbitingParticle />
+                <OrbitingParticle />
+                <OrbitingParticle />
+                <OrbitingParticle />
+                <OrbitingParticle />
+                <FloatingParticle />
+                <FloatingParticle />
+                <FloatingParticle />
+                <FloatingParticle />
+                <FloatingParticle />
+                <FloatingParticle />
+                <ClickText click={click}>click here</ClickText>
+            </ParticleButton>
 
-            <Contact target="_blank" href="mailto:codebucks27@gmail.com">
+            <Contact target="_blank" href="mailto:vakr3628@colorado.edu">
                 <motion.h2
                 initial={{
                     y:-200,
